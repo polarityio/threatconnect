297c346d6a940971b8fafc524d96b2bd72c66cb9
Ed <ed@polarity.io>
2018-12-14 22:03:43 -0500

    # Conflicts: #	integration.js #	threatconnect.js

--- components/threatconnect.js
+++ components/threatconnect.js
@@ -1,6 +1,186 @@
 'use strict';
 
 polarity.export = PolarityComponent.extend({
+  newTagValue: '',
+  showFalsePositiveAlreadyReported: false,
+  timezone: Ember.computed('Intl', function() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  }),
+  _flashError: function(msg) {
+    this.get('flashMessages').add({
+      message: 'ThreatConnect: ' + msg,
+      type: 'unv-danger',
+      timeout: 3000
+    });
+  },
+  actions: {
+    saveConfidence(orgData, orgDataIndex) {
+      console.info('Saving Confidence');
+      let self = this;
 
+      self.set('block.isLoadingDetails', true);
+      const payload = {
+        action: 'SET_CONFIDENCE',
+        data: {
+          indicatorValue: orgData.meta.indicatorValue,
+          indicatorType: orgData.meta.indicatorType,
+          owner: orgData.owner.name,
+          confidence: orgData.confidence
+        }
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then(
+          function(result) {
+            self.set('block.data.details.' + orgDataIndex + '.confidenceHuman', result.confidenceHuman);
+          },
+          function(err) {
+            console.error(err);
+            self._flashError(err.meta.detail, 'error');
+          }
+        )
+        .finally(() => {
+          self.set('block.isLoadingDetails', false);
+        });
+    },
+    addTag(orgData, orgDataIndex) {
+      let self = this;
+
+      const newTag = this.get('newTagValue').trim();
+      if (newTag.length === 0) {
+        this.set('actionMessage', 'You must enter a tag');
+        return;
+      }
+
+      self.set('block.isLoadingDetails', true);
+      const payload = {
+        action: 'ADD_TAG',
+        data: {
+          indicatorValue: orgData.meta.indicatorValue,
+          indicatorType: orgData.meta.indicatorType,
+          owner: orgData.owner.name,
+          tag: newTag
+        }
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then(
+          function() {
+            self.set('actionMessage', 'Added Tag');
+            self.get('block.data.details.' + orgDataIndex + '.tag').pushObject({
+              name: newTag,
+              webLink: ''
+            });
+          },
+          function(err) {
+            console.error(err);
+            self._flashError(err.meta.detail, 'error');
+          }
+        )
+        .finally(() => {
+          self.set('newTagValue', '');
+          self.set('block.isLoadingDetails', false);
+        });
+    },
+    deleteTag(tag, orgData, orgDataIndex, tagIndex) {
+      let self = this;
+
+      self.set('block.isLoadingDetails', true);
+      const payload = {
+        action: 'DELETE_TAG',
+        data: {
+          indicatorValue: orgData.meta.indicatorValue,
+          indicatorType: orgData.meta.indicatorType,
+          owner: orgData.owner.name,
+          tag: tag
+        }
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then(
+          function(result) {
+            self.set('actionMessage', 'Deleted Tag');
+            const newTags = [];
+            let tags = self.get('block.data.details.' + orgDataIndex + '.tag');
+            tags.forEach(function(tag, index) {
+              if (index !== tagIndex) {
+                newTags.push(tag);
+              }
+            });
+
+            self.set('block.data.details.' + orgDataIndex + '.tag', newTags);
+          },
+          function(err) {
+            console.error(err);
+            self._flashError(err.meta.detail, 'error');
+          }
+        )
+        .finally(() => {
+          self.set('block.isLoadingDetails', false);
+        });
+    },
+    reportFalsePositive(orgData, orgDataIndex) {
+      let self = this;
+
+      self.set('block.isLoadingDetails', true);
+      const payload = {
+        action: 'REPORT_FALSE_POSITIVE',
+        data: {
+          indicatorValue: orgData.meta.indicatorValue,
+          indicatorType: orgData.meta.indicatorType,
+          owner: orgData.owner.name
+        }
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then(
+          function(result) {
+            if (self.get('block.data.details.' + orgDataIndex + '.falsePositiveCount') === result.count) {
+              self.set('showFalsePositiveAlreadyReported', true);
+            } else {
+              self.set('showFalsePositiveAlreadyReported', false);
+            }
+            self.set('block.data.details.' + orgDataIndex + '.falsePositiveLastReported', result.lastReported);
+            self.set('block.data.details.' + orgDataIndex + '.falsePositiveCount', result.count);
+          },
+          function(err) {
+            console.error(err);
+            self._flashError(err.meta.detail, 'error');
+          }
+        )
+        .finally(() => {
+          self.set('block.isLoadingDetails', false);
+        });
+    },
+    setRating(orgData, orgDataIndex, rating) {
+      let self = this;
+
+      self.set('block.isLoadingDetails', true);
+      const payload = {
+        action: 'SET_RATING',
+        data: {
+          indicatorValue: orgData.meta.indicatorValue,
+          indicatorType: orgData.meta.indicatorType,
+          owner: orgData.owner.name,
+          rating: rating
+        }
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then(
+          function(result) {
+            self.set('actionMessage', 'Set rating to : ' + rating);
+            self.set('block.data.details.' + orgDataIndex + '.rating', rating);
+            self.set('block.data.details.' + orgDataIndex + '.ratingHuman', result.ratingHuman);
+          },
+          function(err) {
+            console.error(err);
+            self._flashError(err.meta.detail, 'error');
+          }
+        )
+        .finally(() => {
+          self.set('block.isLoadingDetails', false);
+        });
+    }
+  }
 });
-
--- integration.js
+++ integration.js
@@ -1,231 +1,372 @@
 'use strict';
 
-let ipaddr = require('ipaddr.js');
-let async = require('async');
-let url = require('url');
-let ThreatConnect = require('./threatconnect');
-let request = require('request');
+const ipaddr = require('ipaddr.js');
+const async = require('async');
+const url = require('url');
+const ThreatConnect = require('./threatconnect');
+const request = require('request');
 const fs = require('fs');
 const config = require('./config/config');
 const MAX_SUMMARY_TAGS = 6;
+
 let tc;
 let Logger;
 
 function startup(logger) {
-    Logger = logger;
+  Logger = logger;
 
-    let defaults = {};
+  let defaults = {};
 
-    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
-        defaults.cert = fs.readFileSync(config.request.cert);
-    }
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+    defaults.cert = fs.readFileSync(config.request.cert);
+  }
 
-    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
-        defaults.key = fs.readFileSync(config.request.key);
-    }
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+    defaults.key = fs.readFileSync(config.request.key);
+  }
 
-    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
-        defaults.passphrase = config.request.passphrase;
-    }
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+    defaults.passphrase = config.request.passphrase;
+  }
 
-    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
-        defaults.ca = fs.readFileSync(config.request.ca);
-    }
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+    defaults.ca = fs.readFileSync(config.request.ca);
+  }
 
-    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
-        defaults.proxy = config.request.proxy;
-    }
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+    defaults.proxy = config.request.proxy;
+  }
 
-    if (typeof config.request.rejectUnauthorized === 'boolean') {
-        defaults.rejectUnauthorized = config.request.rejectUnauthorized;
-    }
-
-    tc = new ThreatConnect(request.defaults(defaults), Logger);
+  tc = new ThreatConnect(request.defaults(defaults), Logger);
 }
 
 function doLookup(entities, options, cb) {
-    let lookupResults = [];
+  let lookupResults = [];
 
-    tc.setSecretKey(options.apiKey);
-    tc.setHost(options.url);
-    tc.setAccessId(options.accessId);
+  tc.setSecretKey(options.apiKey);
+  tc.setHost(options.url);
+  tc.setAccessId(options.accessId);
 
-    Logger.trace({entities: entities}, 'doLookup');
+  Logger.trace({ entities: entities }, 'doLookup');
 
-    let organizations = [];
+  let organizations = [];
 
-    if (typeof options.defaultOrganizations === 'string' &&
-        options.defaultOrganizations.trim().length > 0) {
-        let tokens = options.defaultOrganizations.split(',');
-        tokens.forEach(token => {
-            token = token.trim();
-            if (token.length > 0) {
-                organizations.push(token);
-            }
-        });
-    } else {
-        organizations.push('');
-    }
-
-    Logger.debug({organizations: organizations});
-
-    async.each(entities, function (entityObj, next) {
-        _lookupEntity(entityObj, organizations, options, function (err, entityResults) {
-            if (err) {
-                next(err);
-                return;
-            }
-
-            lookupResults.push(entityResults);
-            next(null);
-        });
-    }, function (err) {
-        cb(err, lookupResults);
+  if (typeof options.defaultOrganizations === 'string' && options.defaultOrganizations.trim().length > 0) {
+    let tokens = options.defaultOrganizations.split(',');
+    tokens.forEach((token) => {
+      token = token.trim();
+      if (token.length > 0) {
+        organizations.push(token);
+      }
     });
+  } else {
+    organizations.push('');
+  }
+
+  Logger.debug({ organizations: organizations });
+
+  async.each(
+    entities,
+    function(entityObj, next) {
+      _lookupEntity(entityObj, organizations, options, function(err, entityResults) {
+        if (err) {
+          next(err);
+          return;
+        }
+
+        lookupResults.push(entityResults);
+        next(null);
+      });
+    },
+    function(err) {
+      Logger.debug({ lookupResults }, 'Lookup Results');
+      cb(err, lookupResults);
+    }
+  );
 }
 
 function _lookupEntity(entityObj, organizations, options, cb) {
-    let orgResults = {
-        entity: entityObj,
-        data: {
-            summary: [],
-            details: []
+  let orgResults = {
+    entity: entityObj,
+    data: {
+      summary: [],
+      details: []
+    }
+  };
+
+  async.each(
+    organizations,
+    function(org, next) {
+      _lookupOrg(entityObj, org, options, function(err, data) {
+        if (err) {
+          next(err);
+          return;
         }
-    };
 
-    async.each(organizations, function (org, next) {
-        _lookupOrg(entityObj, org, options, function (err, data) {
-            if (err) {
-                next(err);
-                return;
-            }
+        Logger.debug({ data }, 'Tag Information');
+        if (data) {
+          _getSummaryTags(data).forEach(function(tag) {
+            orgResults.data.summary.push(tag);
+          });
 
-            if (data) {
-                _getSummaryTags(data).forEach(function (tag) {
-                    orgResults.data.summary.push(tag);
-                });
+          // data.webLink = _formatWebLink(data.webLink);
 
-                data.webLink = _formatWebLink(data.webLink);
-
-                orgResults.data.details.push(data);
-            }
-
-            next(null);
-        });
-    }, function (err) {
-        if (orgResults.data.details.length === 0) {
-            cb(err, {entity: entityObj, data: null});
-        } else {
-            cb(err, orgResults);
+          orgResults.data.details.push(data);
         }
-    });
+
+        next(null);
+      });
+    },
+    function(err) {
+      if (orgResults.data.details.length === 0) {
+        cb(err, { entity: entityObj, data: null });
+      } else {
+        cb(err, orgResults);
+      }
+    }
+  );
 }
 
 function _lookupOrg(entityObj, org, options, cb) {
-    //Logger.info({value: entityObj.value, org:org},'Lookup');
-    //Logger.debug({options:options}, 'Lookup Options');
+  //Logger.info({value: entityObj.value, org:org},'Lookup');
+  //Logger.debug({options:options}, 'Lookup Options');
 
-    // make a copy of the value so when we modify it we aren't changing the original
-    // entityObj.  This will make caching of the entityObj value more consistent.
-    let lookupValue = entityObj.value;
+  // make a copy of the value so when we modify it we aren't changing the original
+  // entityObj.  This will make caching of the entityObj value more consistent.
+  let lookupValue = entityObj.value;
 
-    if ((entityObj.isIPv4 || entityObj.isIPv6) && options.lookupIps) {
-        // TC does not recognize fully expanded IPv6 addresses
-        // TC does not recognize leading zeroes in IPv6 address octets
-        // TC does not recognize IPv6 addresses unless they are lowercase
-        // TC does not recognize IPv6 addresses if they use the "compressed" :: form for zeroes
-        if (entityObj.isIPv6) {
-            if(ipaddr.isValid(lookupValue)){
-                // convert the IPv6 address into a format TC understands
-                lookupValue = ipaddr.parse(lookupValue).toNormalizedString();
-            }else{
-                cb('Integration Received an invalid IPv6 address [' + lookupValue + ']');
-                return;
-            }
-        }
-
-        Logger.debug({value: lookupValue, org: org}, 'IP Lookup (after IPv6 cleanup to support TC)');
-
-        tc.getAddress(lookupValue, org, function (err, orgData) {
-            if (err) {
-                Logger.error({err: err}, 'Could not retrieve IP info');
-                cb(err);
-                return;
-            }
-
-            cb(null, orgData, lookupValue);
-        });
-    } else if (entityObj.isEmail && options.lookupEmails) {
-        tc.getEmail(lookupValue, org, function (err, orgData) {
-            if (err) {
-                Logger.error({err: err}, 'Could not retrieve email info');
-                cb(err);
-                return;
-            }
-
-            cb(null, orgData);
-        });
-    } else if (entityObj.isHash && options.lookupHashes) {
-        tc.getFile(lookupValue, org, function (err, orgData) {
-            if (err) {
-                Logger.error({err: err}, 'Could not retrieve hash info');
-                cb(err);
-                return;
-            }
-
-            cb(null, orgData);
-        });
-    } else if (entityObj.isDomain && options.lookupHosts) {
-        tc.getHost(lookupValue, org, function (err, orgData) {
-            if (err) {
-                Logger.error({err: err}, 'Could not retrieve host info');
-                cb(err);
-                return;
-            }
-
-            cb(null, orgData);
-        });
-    } else {
-        cb(null);
+  if (entityObj.isIPv4 || entityObj.isIPv6) {
+    // TC does not recognize fully expanded IPv6 addresses
+    // TC does not recognize leading zeroes in IPv6 address octets
+    // TC does not recognize IPv6 addresses unless they are lowercase
+    // TC does not recognize IPv6 addresses if they use the "compressed" :: form for zeroes
+    if (entityObj.isIPv6) {
+      if (ipaddr.isValid(lookupValue)) {
+        // convert the IPv6 address into a format TC understands
+        lookupValue = ipaddr.parse(lookupValue).toNormalizedString();
+      } else {
+        cb('Integration Received an invalid IPv6 address [' + lookupValue + ']');
+        return;
+      }
     }
+
+    Logger.debug({ value: lookupValue, org: org }, 'IP Lookup (after IPv6 cleanup to support TC)');
+
+    tc.getAddressTags(lookupValue, org, function(err, orgData) {
+      if (err) {
+        Logger.error({ err: err }, 'Could not retrieve IP info');
+        cb(err);
+        return;
+      }
+
+      cb(null, orgData, lookupValue);
+    });
+  } else if (entityObj.isEmail) {
+    tc.getEmailTags(lookupValue, org, function(err, orgData) {
+      if (err) {
+        Logger.error({ err: err }, 'Could not retrieve email info');
+        cb(err);
+        return;
+      }
+
+      cb(null, orgData);
+    });
+  } else if (entityObj.isHash) {
+    tc.getFileTags(lookupValue, org, function(err, orgData) {
+      if (err) {
+        Logger.error({ err: err }, 'Could not retrieve hash info');
+        cb(err);
+        return;
+      }
+
+      cb(null, orgData);
+    });
+  } else if (entityObj.isDomain) {
+    tc.getHostTags(lookupValue, org, function(err, orgData) {
+      if (err) {
+        Logger.error({ err: err }, 'Could not retrieve host info');
+        cb(err);
+        return;
+      }
+
+      cb(null, orgData);
+    });
+  } else {
+    cb(null);
+  }
+}
+
+function onDetails(lookupObject, options, cb) {
+  Logger.debug({ lookupObject }, 'onDetails Input');
+  const details = lookupObject.data.details;
+  const tasks = [];
+
+  tc.setSecretKey(options.apiKey);
+  tc.setHost(options.url);
+  tc.setAccessId(options.accessId);
+
+  details.forEach((org) => {
+    tasks.push(function(done) {
+      tc.getIndicator(org.meta.indicatorType, org.meta.indicatorValue, org.owner, done);
+    });
+  });
+
+  async.parallel(tasks, (err, results) => {
+    if (err) {
+      cb(err);
+    } else {
+      Logger.debug({ results: results }, 'onDetails Results');
+      results.forEach((result) => {
+        if (result.threatAssessScore) {
+          result.threatAssessScorePercentage = (result.threatAssessScore / 1000) * 100;
+        } else {
+          result.threatAssessScorePercentage = 0;
+        }
+      });
+      cb(null, {
+        summary: lookupObject.data.summary,
+        details: results
+      });
+    }
+  });
+}
+
+function onMessage(payload, options, cb) {
+  Logger.debug({ payload }, 'Received onMessage');
+  tc.setSecretKey(options.apiKey);
+  tc.setHost(options.url);
+  tc.setAccessId(options.accessId);
+
+  switch (payload.action) {
+    case 'SET_RATING':
+      tc.setRating(
+        payload.data.indicatorValue,
+        payload.data.indicatorType,
+        payload.data.owner,
+        payload.data.rating,
+        function(err, result) {
+          if (err) {
+            Logger.error({ err, payload }, 'Error Setting Rating');
+            cb(err);
+          } else {
+            Logger.debug({ result: result }, 'Returning SET_RATING');
+            cb(null, result);
+          }
+        }
+      );
+      break;
+    case 'SET_CONFIDENCE':
+      tc.setConfidence(
+        payload.data.indicatorValue,
+        payload.data.indicatorType,
+        payload.data.owner,
+        payload.data.confidence,
+        function(err, result) {
+          if (err) {
+            Logger.error({ err, payload }, 'Error Setting Rating');
+            cb(err);
+          } else {
+            Logger.debug({ result: result }, 'Returning SET_CONFIDENCE');
+            cb(null, result);
+          }
+        }
+      );
+      break;
+    case 'REPORT_FALSE_POSITIVE':
+      tc.reportFalsePositive(
+        payload.data.indicatorType,
+        payload.data.indicatorValue,
+        payload.data.owner,
+        (err, result) => {
+          if (err) {
+            Logger.error({ err, payload }, 'Error Reporting False Positive');
+            cb(err);
+          } else {
+            Logger.debug({ result: result }, 'Returning REPORT_FALSE_POSITIVE');
+            cb(null, result);
+          }
+        }
+      );
+      break;
+    case 'DELETE_TAG':
+      tc.deleteTag(
+        payload.data.indicatorType,
+        payload.data.indicatorValue,
+        payload.data.tag,
+        payload.data.owner,
+        (err) => {
+          if (err) {
+            Logger.error({ err, payload }, 'Error Deleting Tag');
+            cb(err);
+          } else {
+            Logger.debug('Returning DELETE_TAG');
+            cb(null, {});
+          }
+        }
+      );
+      break;
+    case 'ADD_TAG':
+      tc.addTag(
+        payload.data.indicatorType,
+        payload.data.indicatorValue,
+        payload.data.tag,
+        payload.data.owner,
+        (err) => {
+          if (err) {
+            Logger.error({ err, payload }, 'Error Adding Tag');
+            cb(err);
+          } else {
+            Logger.debug('Returning ADD_TAG');
+            cb(null, {});
+          }
+        }
+      );
+      break;
+    default:
+      cb({
+        detail: "Invalid 'action' provided to onMessage function"
+      });
+  }
 }
 
 function _formatWebLink(weblink) {
-    let weblinkAsUrl = url.parse(weblink);
+  let weblinkAsUrl = url.parse(weblink);
 
-    if (config.settings.threatConnectPort !== null) {
-        weblinkAsUrl.port = config.settings.threatConnectPort;
-        // We need to delete the host so that url.format() will recreate
-        // it and include the threatConnectPort
-        delete weblinkAsUrl.host;
-    }
+  if (config.settings.threatConnectPort !== null) {
+    weblinkAsUrl.port = config.settings.threatConnectPort;
+    // We need to delete the host so that url.format() will recreate
+    // it and include the threatConnectPort
+    delete weblinkAsUrl.host;
+  }
 
-    //Logger.info({url: url.format(weblinkAsUrl), port: weblinkAsUrl.port}, 'WebLink');
+  //Logger.info({url: url.format(weblinkAsUrl), port: weblinkAsUrl.port}, 'WebLink');
 
-    return url.format(weblinkAsUrl);
+  return url.format(weblinkAsUrl);
 }
 
 function _getSummaryTags(data) {
-    let summaryTags = [];
+  let summaryTags = [];
 
-    if (data.owner && data.owner.name) {
-        summaryTags.push('<i class="bts bt-building integration-text-bold-color"></i> ' + data.owner.name);
+  if (data.owner && data.owner.name) {
+    summaryTags.push('<i class="bts bt-building integration-text-bold-color"></i> ' + data.owner.name);
+  }
+
+  if (Array.isArray(data.tags)) {
+    for (let i = 0; i < data.tags.length && i <= MAX_SUMMARY_TAGS; i++) {
+      let tag = data.tags[i];
+      summaryTags.push(tag.name);
     }
 
-    if (Array.isArray(data.tags)) {
-        for (let i = 0; i < data.tags.length && i <= MAX_SUMMARY_TAGS; i++) {
-            let tag = data.tags[i];
-            summaryTags.push(tag.name);
-        }
-
-        if (data.tags.length > MAX_SUMMARY_TAGS) {
-            summaryTags.push('+' + (data.tags.length - MAX_SUMMARY_TAGS));
-        }
+    if (data.tags.length > MAX_SUMMARY_TAGS) {
+      summaryTags.push('+' + (data.tags.length - MAX_SUMMARY_TAGS));
     }
-    return summaryTags;
+  }
+  return summaryTags;
 }
 
 module.exports = {
-    doLookup: doLookup,
-    startup: startup
+  doLookup: doLookup,
+  startup: startup,
+  onMessage: onMessage,
+  onDetails: onDetails
 };
