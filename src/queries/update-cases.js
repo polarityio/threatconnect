029768cf672f3fd120876b160351e07c9fdf1944
Adriana Haias <ahaias@threatconnect.com>
2025-03-21 12:14:11 +0200

    Fix for case updates

--- src/queries/update-cases.js
+++ src/queries/update-cases.js
@@ -1,6 +1,6 @@
-const polarityRequest = require('../polarity-request');
-const { ApiRequestError, IntegrationError } = require('../errors');
-const { getLogger } = require('../logger');
+const polarityRequest = require("../polarity-request");
+const { ApiRequestError, IntegrationError } = require("../errors");
+const { getLogger } = require("../logger");
 const SUCCESS_CODES = [200];
 
 /**
@@ -13,16 +13,16 @@ const SUCCESS_CODES = [200];
  * @returns {Promise<{}>}
  */
 async function updateCaseTags(caseId, tag, mode, options) {
-  if (mode !== 'append' && mode !== 'delete') {
+  if (mode !== "append" && mode !== "delete") {
     throw new IntegrationError(`Invalid mode provided to updateTag method. Supported modes are "append" and "delete"`);
   }
   const Logger = getLogger();
 
   const requestOptions = {
     uri: `${options.url}/v3/cases/${caseId}`,
-    method: 'PUT',
+    method: "PUT",
     qs: {
-      fields: 'tags'
+      fields: "tags"
     },
     body: {}
   };
@@ -36,17 +36,17 @@ async function updateCaseTags(caseId, tag, mode, options) {
     mode
   };
 
-  Logger.trace({ requestOptions }, 'Request Options');
+  Logger.trace({ requestOptions }, "Request Options");
 
   const apiResponse = await polarityRequest.request(requestOptions, options);
 
-  Logger.trace({ apiResponse }, 'Update Case Tags API Response');
+  Logger.trace({ apiResponse }, "Update Case Tags API Response");
 
-  if (SUCCESS_CODES.includes(apiResponse.statusCode) && apiResponse.body && apiResponse.body.status === 'Success') {
+  if (SUCCESS_CODES.includes(apiResponse.statusCode) && apiResponse.body && apiResponse.body.status === "Success") {
     return apiResponse.body.data;
   } else if (
     !SUCCESS_CODES.includes(apiResponse.statusCode) ||
-    (apiResponse.body && apiResponse.body.status && apiResponse.body.status !== 'Success')
+    (apiResponse.body && apiResponse.body.status && apiResponse.body.status !== "Success")
   ) {
     throw new ApiRequestError(
       `Unexpected status code ${apiResponse.statusCode} received when making request to the ThreatConnect API`,
@@ -67,64 +67,54 @@ async function updateCaseTags(caseId, tag, mode, options) {
  * @returns {Promise<{}>}
  */
 async function updateCase(payload, options) {
-  if (payload.mode !== 'append' && payload.mode !== 'delete') {
+  if (payload.mode !== "append" && payload.mode !== "delete") {
     throw new IntegrationError(`Invalid mode provided to updateCase method. Supported modes are "append" and "delete"`);
   }
   const Logger = getLogger();
 
   const requestOptions = {
     uri: `${options.url}/v3/cases/${payload.caseId}`,
-    qs: { fields: 'attributes' },
-    method: 'PUT',
+    qs: { fields: "attributes" },
+    method: "PUT",
     body: {}
   };
 
   const status = payload.status;
-  if (status && status !== 'undefined') {
-    requestOptions.body = {
-      status: status
-    };
+  if (status && status !== "undefined") {
+    requestOptions.body.status = status;
   }
 
   const severity = payload.severity;
-  if (severity && severity !== 'undefined') {
-    requestOptions.body = {
-      severity: severity
-    };
+  if (severity && severity !== "undefined") {
+    requestOptions.body.severity = severity;
   }
 
   const resolution = payload.resolution;
-  if (resolution && resolution !== 'undefined') {
-    requestOptions.body = {
-      resolution: resolution
-    };
+  if (resolution && resolution !== "undefined") {
+    requestOptions.body.resolution = resolution;
   }
 
   const description = payload.description;
-  if (description && description !== 'undefined') {
-    requestOptions.body = {
-      description: description
-    };
+  if (description && description !== "undefined") {
+    requestOptions.body.description = description;
   }
 
   const attributes = payload.attributes;
-  if (attributes && attributes !== 'undefined') {
-    requestOptions.body = {
-      attributes: attributes
-    };
+  if (attributes && attributes !== "undefined") {
+    requestOptions.body.attributes = attributes;
   }
 
-  Logger.trace({ requestOptions }, 'Request Options');
+  Logger.trace({ requestOptions }, "Request Options");
 
   const apiResponse = await polarityRequest.request(requestOptions, options);
 
-  Logger.trace({ apiResponse }, 'Update Case API Response');
+  Logger.trace({ apiResponse }, "Update Case API Response");
 
-  if (SUCCESS_CODES.includes(apiResponse.statusCode) && apiResponse.body && apiResponse.body.status === 'Success') {
+  if (SUCCESS_CODES.includes(apiResponse.statusCode) && apiResponse.body && apiResponse.body.status === "Success") {
     return apiResponse.body.data;
   } else if (
     !SUCCESS_CODES.includes(apiResponse.statusCode) ||
-    (apiResponse.body && apiResponse.body.status && apiResponse.body.status !== 'Success')
+    (apiResponse.body && apiResponse.body.status && apiResponse.body.status !== "Success")
   ) {
     throw new ApiRequestError(
       `Unexpected status code ${apiResponse.statusCode} received when making request to the ThreatConnect API`,
